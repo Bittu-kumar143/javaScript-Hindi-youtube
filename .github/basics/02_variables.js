5a021849350ec2c64eb7cf4234e9097a1935c5fb
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2025-12-22 19:20:25 +0000

    javascript

---
+++ .github/basics/02_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "nitj.ac.in"
+let accountPassword = "12341"
+accountCity = "jaipur"
+let accountState;
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
