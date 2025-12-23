e6d480e6bf282249947c2b774d7c026a3cf5e656
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2025-12-23 09:49:17 +0000

    conversion of data types

---
+++ .github/basics/04_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+console.log(typeof score)
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+
+
+
+let number = "22"
+let StringNumber = String(number)
+console.log(StringNumber)
+console.log(typeof StringNumber)
