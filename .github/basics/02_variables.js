cc47ae6ba316584db1161d2751720a7121593d02
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2025-12-24 14:06:45 +0000

    Removed .github/basics folder

--- .github/basics/02_variables.js
+++
@@ -1,7 +0,0 @@
-const accountId = 144553
-let accountEmail = "nitj.ac.in"
-let accountPassword = "12341"
-accountCity = "jaipur"
-let accountState;
-console.log(accountId)
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
--- 05_string.js
+++
@@ -1,4 +0,0 @@
-let name = "Bittu"
-let repocount = 50
-
-console.log(name + repocount + "value");
