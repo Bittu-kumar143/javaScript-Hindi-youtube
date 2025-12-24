95f5a1f6b213932fa06708315a9ab179eb08c6bc
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2025-12-24 17:00:50 +0000

    strings

--- basics/05_strings.js
+++ basics/05_strings.js
@@ -5,13 +5,31 @@ let repocount = 50
 
 console.log(`hello my name is ${name} and my repo count is ${repocount}`);
 
-const gamename = new String('Bittu')
+const gamename = new String('Hitesh')
 
 console.log(gamename[0]);
 console.log(gamename.length);
 console.log(gamename.toUpperCase());
 console.log(gamename.charAt(2));
 console.log(gamename.indexOf('t'));
+
+const newstring  = gamename.substring(0,4)
+console.log(newstring);
+
+const anotherstring  = gamename.slice(-7,5)
+console.log(anotherstring);
+
+const newstringone = "     bittuKumar   "
+console.log(newstringone);
+console.log(newstringone.trim());
+
+
+const url = "https://bittu%20google.com"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sunder'));
+
+console.log(gamename.split('t'));
 
 
 
