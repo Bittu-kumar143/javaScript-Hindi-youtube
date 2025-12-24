62f3441f44d15c27eb013289df0708d0e5e23ed4
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2025-12-24 16:25:47 +0000

    string

--- basics/05_strings.js
+++ basics/05_strings.js
@@ -1,4 +1,17 @@
 let name = "Bittu"
 let repocount = 50
 
-console.log(name + repocount + "value");
+//console.log(name + repocount + "value"); //This is not a good practice to write
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename = new String('Bittu')
+
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('t'));
+
+
+
