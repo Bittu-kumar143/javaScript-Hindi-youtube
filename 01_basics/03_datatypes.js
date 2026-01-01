5dc1e268c8404569d82d9ca71341918c694ae4bf
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-01 07:54:28 +0000

    commit

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,21 @@
+let name  = "Bittu"
+let Age = 25
+let isLoggedIn = false
+let state = null
+
+//number
+//bigInt
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
