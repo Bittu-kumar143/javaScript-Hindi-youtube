a6ad7e0535bd7e941ef4a88a5f61a691a97c5171
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-04 16:22:42 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,33 @@
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "hello world!"
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`);
+}
+
+
+//Maps
+const map = new Map()
+map.set(`IN`,"INDIA")
+map.set(`USA`,"UNITED STATES OF AMERICA")
+map.set(`FR`,"FRANCE")
+map.set(`IN`,"INDIA")
+
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key,value);
+}
+const myobject = {
+    'game 1' : 'NFS',
+    'game 2' : 'SPIDERMAN',
+}
+
+for (const [key,value] of myobject) {
+    console.log(key , value);
+}
+
