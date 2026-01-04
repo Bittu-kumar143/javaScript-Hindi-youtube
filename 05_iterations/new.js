26de82ff973b39e629f25b9ff4942a81fc97f5fb
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-04 15:48:53 +0000

    loop

--- 05_iterations/new.js
+++ 05_iterations/new.js
@@ -9,3 +9,22 @@ for (let i = 0; i < 10; i++) {
         const element = array[i];
     }
 }*/
+
+ // break and continue
+ /*for (let index = 0; index <= 20; index++) {
+    if(index == 5){
+        console.log(`detected 5`);
+        break
+    }
+    console.log(`value of i is ${index}`);
+ }*/
+
+
+ for (let index = 0; index <= 20; index++) {
+    if(index == 5){
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+ }
+
