c84d1daa1e94f9f9db91202cb91c46abcc02d61f
Bittu kumar <124612467+Bittu-kumar143@users.noreply.github.com>
2026-01-04 15:39:06 +0000

    loops

---
+++ 05_iterations/new.js
@@ -0,0 +1,11 @@
+//for 
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element);
+}
+
+/*for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+        const element = array[i];
+    }
+}*/
